67227ebf68b67c0f6dfb98d07264b08ba47f7442
Kajetan Krason <kajetan.krason@estigiti.com>
2024-12-22 23:18:24 +0100

    Add clock display and update weather UI logic
    Introduced a live clock feature with automatic updates using Fyne widgets. Refactored weather fetching and display functionality for better modularity and integrated it into a Fyne GUI application. Updated `go.mod` to include necessary dependencies for the changes.

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 	owm "github.com/briandowns/openweathermap"
 	dotenv "github.com/joho/godotenv"
 	"os"
+	"time"
 )
 
 type Coordinates struct {
@@ -27,19 +31,37 @@ func checkError(e error) {
 	}
 }
 
-func main() {
+func updateTime(clock *widget.Label) {
+	formatted := time.Now().Format("Time: 03:04:05")
+	clock.SetText(formatted)
+}
+
+func setupClock() *widget.Label {
+	clock := widget.NewLabel("")
+	updateTime(clock)
+
+	go func() {
+		for range time.Tick(time.Second) {
+			updateTime(clock)
+		}
+	}()
+
+	return clock
+}
+
+func loadEnv() {
 	err := dotenv.Load()
 	checkError(err)
+}
 
-	owmApiKey := os.Getenv("OWM_API_KEY")
-
-	w, err := owm.NewCurrent("C", "PL", owmApiKey) // fahrenheit (imperial) with Russian output
+func fetchWeather(apiKey, location string) Weather {
+	w, err := owm.NewCurrent("C", "PL", apiKey)
 	checkError(err)
 
-	err = w.CurrentByName("Nowa Sól,PL")
+	err = w.CurrentByName(location)
 	checkError(err)
 
-	weather := Weather{
+	return Weather{
 		placeName:   w.Name,
 		temperature: w.Main.Temp,
 		description: w.Weather[0].Description,
@@ -50,13 +72,36 @@ func main() {
 		humidity:  w.Main.Humidity,
 		windSpeed: w.Wind.Speed,
 	}
+}
 
-	fmt.Printf("Miejsce: %s\n", weather.placeName)
-	fmt.Printf("Temperatura: %.2f°C\n", weather.temperature)
-	fmt.Printf("Opis pogody: %s\n", weather.description)
-	fmt.Printf("Koordynaty (długość, szerokość): (%.3f, %.3f)\n",
+func displayWeather(weather Weather, label *widget.Label) {
+	formattedWeather := fmt.Sprintf(
+		"Miejsce: %s\nTemperatura: %.2f°C\nOpis: %s\nWilgotność: %d%%\nWiatr: %.2f m/s\nKoordynaty: (%.3f, %.3f)",
+		weather.placeName,
+		weather.temperature,
+		weather.description,
+		weather.humidity,
+		weather.windSpeed,
 		weather.coordinates.Longitude,
-		weather.coordinates.Latitude)
-	fmt.Printf("Wilgotność: %d%%\n", weather.humidity)
-	fmt.Printf("Prędkość wiatru: %.2f m/s\n", weather.windSpeed)
+		weather.coordinates.Latitude,
+	)
+	label.SetText(formattedWeather)
+}
+
+func main() {
+	a := app.New()
+	win := a.NewWindow("Clock & Weather")
+
+	clock := setupClock()
+
+	loadEnv()
+	weatherLabel := widget.NewLabel("Loading Weather Information...")
+	owmApiKey := os.Getenv("OWM_API_KEY")
+	weather := fetchWeather(owmApiKey, "Złocieniec,PL")
+	displayWeather(weather, weatherLabel)
+
+	content := container.NewVBox(clock, weatherLabel)
+
+	win.SetContent(content)
+	win.ShowAndRun()
 }
